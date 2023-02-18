de6ba0f7d8a22bf6d5a34e2847cfcc50545aadde
walternewtz <walternewtz@outlook.com>
2023-02-18 14:52:05 +0530

    a

--- k.sh
+++ k.sh
@@ -8,6 +8,6 @@ cd kernelsu && sed -i 's/0x033b/0x36c/g' Makefile && sed -i 's/0xb0b91415/0x4da8
 cd .. && cd ..
 git add .
 git commit -m "a"
-git reset --soft HEAD~2
-git commit -m "add kernelsu"
+#git reset --soft HEAD~2
+#git commit -m "add kernelsu"
 git push -f
